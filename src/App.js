265758b225c019daa47bb03e773bfbca5686cc65
raghava-77 <palluraghava99@gmail.com>
2021-09-23 12:37:56 -0700

    0k

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ function App(){
+    return <> 
+   
+    <h1 style={{"background-color":'red'}} >hi hello how are you </h1>
+    
+
+      
+      
+       </>
+}
+export default App;
