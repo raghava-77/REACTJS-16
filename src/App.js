e7e1be7d5187e11f1d164defa06424a918e4e3cd
raghava-77 <palluraghava99@gmail.com>
2021-10-04 17:44:07 -0700

    tables

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 // import Tablestate from './state/table/Tablestate'
-import Constrctor from './eventandbinding/Constrctor'
+// import Constrctor from './eventandbinding/Constrctor'
+// import Emp from './tables/Emp'
+import Emp1 from './tables/empdata2/Emp1'
 function App(){
    return( <>
     {/* <Tablestate /> */}
-    <Constrctor />
+    <Emp1 />
    </>
    );
 }
---
+++ src/tables/empdata2/Emp1.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Data from '../Data';
+class Emp1 extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            emp: Data,
+        }
+}
+    render(){
+        return(
+        <>
+        
+
+
+<div className="container mt-4">
+    <div className="row">
+        <div className="col-md-8">
+            <table className="table table-hover">
+                <thead>
+                <tr>
+                   <th>id</th>
+                   <th>name</th>
+                   <th>email</th>
+                   <th>gender</th>
+                </tr>
+                </thead>
+                <tbody>
+                    {this.state.emp.map((emp)=>{
+                        return(
+                        <tr>
+                            <td>{emp.id}</td>
+                            <td>{emp.name}</td>
+                            <td>{emp.email}</td>
+                            <td>{emp.gender}</td>
+                        </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+
+        </>)
+    }
+}
+export default Emp1;
